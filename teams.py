2d54ac7ac46561494241568c48f2b8c36fbcc26a
Yannick Hochstuhl <yannick.hochstuhl@gmx.net>
2020-12-25 15:08:54 +0100

    added comments

--- teams.py
+++ teams.py
@@ -25,8 +25,13 @@ class RoundRobin():
         print(self.player_list)
         
     def generate_schedule(self):
-        #Shuffle raus zum debuggen
+        #shuffle player list to randomly assign start player
         np.random.shuffle(self.player_list)
+
+        #index where array gets flipped to create a circular array and to implement a behaviour like
+        #  1 2 3  -->  1 6 2  -->  1 5 6
+        #  6 5 4       5 4 3       4 3 2
+
         flip_index = self.player_count//2 - 1
         players = self.player_list
         first = players[0]
@@ -37,7 +42,8 @@ class RoundRobin():
         
         
         ################################################################
-        
+        # this loop implements the round robin behaviour with flipping, rolling etc
+        # player count - 2 because the first round is already assigned
         for _ in range(self.player_count - 2):
 
             rest = np.concatenate((rest[:flip_index],np.flip(rest[flip_index:])))
@@ -55,8 +61,8 @@ class RoundRobin():
         
     def stack_csv(self, teams_assigned=True):
         rows, cols = self.schedule.shape
-        header = header = np.array([['Day','Player Home','Team Home','Home Score','Away Score','Away Team','Away Player']])
-        # header = np.array([['Spieltag','Heimspieler','Heimmannschaft','Heimtore','Auswärtstore','Auswärtsmannschaft','Auswärtsspieler']])
+        # header = header = np.array([['Day','Player Home','Team Home','Home Score','Away Score','Away Team','Away Player']])
+        header = np.array([['Spieltag','Heimspieler','Heimmannschaft','Heimtore','Auswärtstore','Auswärtsmannschaft','Auswärtsspieler']])
         day = 0
         if teams_assigned:
             for x in range(2):
@@ -104,19 +110,35 @@ class RoundRobin():
     def assign_teams_once(self):
         debug = 1
         rows, cols = self.schedule.shape
+        # assign empty array for the team schedule
         team_schedule = [[' '*10 for _ in range(cols)] for _ in range(rows)]
+
+        # empty numpy array for first round
         self.team_schedule_hin = np.array(team_schedule)
+        
+        # empty numpy array for first round
         self.team_schedule_rueck = np.array(team_schedule)
+
+        # create a dict containing the players as keys() and the available teams as in a list as values
+        # this is done to keep track of remaining teams for each player
         for i in range(len(self.player_list)):
                     self.player_dict[self.player_list[i][0]] = self.teams
         
         for runde in range(2):
+            # since this is a two round robin tournament if the first round is already assigned
+            # the remaining teams for each player are assigned to a new dict. If in the end there is no
+            # compatible merge of players/teams, only the second half gets assigned again
+
             if runde == 1:
                 player_dict_second_half = self.player_dict.copy()
 
             done = False
             while not done:
                 done = True
+
+                # iterate through the already built schedule, pick a player on a specific gameday, test if this player has non-picked
+                # teams (gameday) available. If he/she has: assign a randomly chosen team to the specific player/game
+                # if not (len(intersect == 0)): start this process all over again 
                 for row in range(0,rows-1, 2):
                     team_list = self.teams
                     for col in range(cols): 
@@ -168,11 +190,10 @@ class RoundRobin():
                         self.player_dict[away_player]= np.delete(self.player_dict[away_player], np.where(self.player_dict[away_player] == pick_away)[0])
                         
 
-
-players = np.array([['Ringo'],['John'],['Paul'],['Yoko'],['George'],['Linda']])
-teams = np.array([['Packers'],['Patriots'],['Vikings'],['Rams'],['49ers'],['Chiefs'],['Dolphins'],['Seahawks'],['Lions'],['Panthers']])
-
+players = np.array([['Bensteiger'],['Jonas'],['Yannik'],['Yps'],['Diet'],['Maxi'],['Max Powers'],['Micha'],['Andi'],['David'] ])
+teams = np.array([['Bayern'],['Dortmund'],['Leverkusen'],['Schalke'],['Hoffenheim'],['Leipzig'],['Stuttgart'],['Freiburg'],['Gladbach'],['Augsburg'],['Union'],['Hertha'],['Bremen'],['Frankfurt'],['Wolfsburg'],['Köln'],['Arminia'],['Mainz']])
 if __name__ == '__main__':
+    
     turnier = RoundRobin(players=players, teams=teams)
     turnier.generate_schedule()
     
